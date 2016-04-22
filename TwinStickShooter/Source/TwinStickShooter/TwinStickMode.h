2bad9a7851725ea02dc3dccccc0108a9828c72c5
antiwb3 <99143631@qq.com>
2016-04-22 17:33:24 +0800

    new
    new

---
+++ TwinStickShooter/Source/TwinStickShooter/TwinStickMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TwinStickMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWINSTICKSHOOTER_API ATwinStickMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
